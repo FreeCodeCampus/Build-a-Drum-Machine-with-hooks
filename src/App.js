323d5aa664178b6e7606f62aa400e1e509ea005b
americanman <snowboardracer7@gmail.com>
2019-07-16 01:23:11 -0400

    Dynamically add buttons on the screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const letters = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+
+const Drum = () => {
+  return (
+    <div id="display" className="container">
+      {letters.map((letter, index) =>
+        (index + 1) % 3 === 0 ? (
+          <>
+            <div className="button" id={letter}>
+              {letter}
+            </div>
+            <div className="break" />
+          </>
+        ) : (
+          <div className="button" id={letter}>
+            {letter}
+          </div>
+        )
+      )}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Drum />
     </div>
   );
 }
