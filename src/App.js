d538518bb46285ba8fff600de70fc15b03eb302d
americanman <snowboardracer7@gmail.com>
2019-07-21 20:44:33 -0400

    Add different sounds for buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from "react";
 import "./App.css";
 
-const letters = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+const letters = [
+  {
+    button: "Q",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    button: "W",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+    button: "E",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    button: "A",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    button: "S",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    button: "D",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    button: "Z",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    button: "X",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    button: "C",
+    track: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
 
 const Drum = () => {
   const sound = e => {
-    const au = document.getElementById("E");
-    console.log(au);
-    au.play();
+    const av = e.target.querySelector("audio");
+    // console.log(e.target, e.target.querySelector("audio").getAttribute("id"));
+    av.play();
   };
 
   return (
@@ -17,31 +54,27 @@ const Drum = () => {
           <>
             <button
               className="drum-pad"
-              id={`cont-${letter}`}
-              onClick={() => sound()}
+              id={`cont-${letter["button"]}`}
+              onClick={e => sound(e)}
             >
               {console.log(letter)}
               <audio
                 className="clip"
-                id={letter}
-                src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+                id={letter["button"]}
+                src={letter.track}
               />
-              {letter}
+              {letter.button}
             </button>
             <div className="break" />
           </>
         ) : (
           <button
             className="drum-pad"
-            id={`cont-${letter}`}
-            onClick={() => sound()}
+            id={`cont-${letter["button"]}`}
+            onClick={e => sound(e)}
           >
-            <audio
-              className="clip"
-              id={letter}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-            />
-            {letter}
+            <audio className="clip" id={letter["button"]} src={letter.track} />
+            {letter.button}
           </button>
         )
       )}
